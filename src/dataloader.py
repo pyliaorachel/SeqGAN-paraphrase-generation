0771f75543aea6f48c30e7d64b3ec9fd8eea55db
Peiyu Liao <u3512485@connect.hku.hk>
2018-08-13 14:39:06 +0800

    Adapt to variable-lengthed input via padding; update everything associated

--- src/dataloader.py
+++ src/dataloader.py
@@ -4,7 +4,7 @@ Sample usage:
     loader.load()
 
     num_samples = 10
-    dataX, dataY, X_lens, Y_lens, end_of_dataset = loader.sample(num_samples)
+    cond_samples, pos_samples, cond_lens, pos_lens, end_of_dataset = loader.sample(num_samples)
 """
 import csv
 
@@ -22,39 +22,53 @@ from . import helpers
 
 
 class DataLoader:
-    def __init__(self, filepath, start_token='<S>', end_token='<E>', pad_token='<P>', gpu=False):
+    def __init__(self, filepath, end_token_str='<E>', pad_token_str='<P>', gpu=False, light_ver=False):
         self.filepath = filepath
-        self.start_token = start_token
-        self.end_token = end_token
-        self.pad_token = pad_token
         self.gpu = gpu
+        self.light_ver = light_ver # Smaller dataset
+        self.end_token_str = end_token_str
+        self.pad_token_str = pad_token_str
 
-        self.dataX = None
-        self.dataY = None
+        self.light_size = 1000
+        self.end_token = 1
+        self.pad_token = 0
+        self.cond_samples = None
+        self.pos_samples = None
+        self.cond_lens = None
+        self.pos_lens = None
         self.word_to_int = None
         self.int_to_word = None
         self.vocab = None
+        self.max_seq_len = 0
 
         self.fetcher = self.fetch()
+        self.frozen = None 
+
+    @property
+    def total_samples(self):
+        return len(self.pos_samples) if self.pos_samples is not None else None
 
     def load(self):
         """
         Load data from dataset. Stores input and target data (padded), word to index mapping, and vocabulary.
         """
         # Build vocab & mapping
-        dataX = []
-        dataY = []
-        vocab = [self.start_token, self.end_token, self.pad_token]
+        cond_samples = []
+        pos_samples = []
+        vocab = [self.end_token_str, self.pad_token_str]
         with open(self.filepath, 'r') as fin:
             fin.readline() # ignore header
             reader = csv.reader(fin, delimiter='\t')
             for row in reader:
+                if self.light_ver and len(cond_samples) >= self.light_size:
+                    break
+
                 # columns: id, qid1, qid2, question1, question2, is_duplicate
                 if row[-1] == '1': # positive examples
-                    q1 = [self.start_token] + nltk.word_tokenize(row[3]) + [self.end_token]
-                    q2 = [self.start_token] + nltk.word_tokenize(row[4]) + [self.end_token]
-                    dataX.append(q1)
-                    dataY.append(q2)
+                    q1 = nltk.word_tokenize(row[3]) + [self.end_token_str]
+                    q2 = nltk.word_tokenize(row[4]) + [self.end_token_str]
+                    cond_samples.append(q1)
+                    pos_samples.append(q2)
                     vocab += nltk.word_tokenize(row[3]) + nltk.word_tokenize(row[4])
 
         self.vocab = sorted(list(set(vocab)))
@@ -62,34 +76,75 @@ class DataLoader:
         self.int_to_word = dict((i, w) for i, w in enumerate(self.vocab))
 
         # Map dataset
-        self.dataX = [self.sent_to_ints(q) for q in dataX]
-        self.dataY = [self.sent_to_ints(q) for q in dataY]
+        self.cond_samples = [self.sent_to_ints(q) for q in cond_samples]
+        self.pos_samples = [self.sent_to_ints(q) for q in pos_samples]
 
-    def sample(self, num_samples):
+        # Map special tokens
+        self.end_token = self.word_to_int[self.end_token_str]
+        self.pad_token = self.word_to_int[self.pad_token_str]
+        
+        # Pad dataset, turn into np array
+        self.cond_samples, self.cond_lens = helpers.pad_samples(self.cond_samples, self.pad_token)
+        self.pos_samples, self.pos_lens = helpers.pad_samples(self.pos_samples, self.pad_token)
+        self.max_seq_len = max(torch.max(self.cond_lens).item(), torch.max(self.pos_lens).item())
+
+    def sample(self, num_samples, is_val=False):
         """
         Samples the dataset, returns num_samples samples of length max_seq_len, wrapped in variables.
         max_seq_len is the max length among all samples; samples shorter than max_seq_len are padded.
+        cond_ids are the corresponding condition id for each positive sample.
+        If is_val, freeze this sampled batch as validation set until release is called.
 
-        Outputs: padded_X, padded_Y, X_lens, Y_lens, end_of_dataset
-            - padded_X, padded_Y: num_samples_fetched x max_seq_len
-            - X_lens, Y_lens: num_samples_fetched
+        Outputs: pos_samples, pos_lens, cond_ids, end_of_dataset
+            - pos_samples: num_samples_fetched x seq_len
+            - pos_lens: num_samples_fetched
+            - cond_ids: num_samples_fetched
         """
         # Fetch next batch
         next(self.fetcher)
-        dataX, dataY, end_of_dataset = self.fetcher.send(num_samples)
+        pos_samples, pos_lens, cond_ids, end_of_dataset = self.fetcher.send(num_samples)
 
-        # Pad sequences
-        pad_token = self.word_to_int[self.pad_token]
-        padded_X, X_lens = helpers.pad_samples(dataX, pad_token)
-        padded_Y, Y_lens = helpers.pad_samples(dataY, pad_token)
+        # Freeze samples if is validation set
+        if is_val:
+            self.freeze(cond_ids[0], cond_ids[-1])
 
         # Wrap in variable
-        padded_X = autograd.Variable(torch.LongTensor(padded_X))
-        padded_Y = autograd.Variable(torch.LongTensor(padded_Y))
-        X_lens = autograd.Variable(torch.LongTensor(X_lens))
-        Y_lens = autograd.Variable(torch.LongTensor(Y_lens))
+        pos_samples = autograd.Variable(torch.LongTensor(pos_samples))
+        pos_lens = autograd.Variable(torch.LongTensor(pos_lens))
 
-        return (padded_X, padded_Y, X_lens, Y_lens, end_of_dataset)
+        if self.gpu:
+            pos_samples = pos_samples.cuda()
+            pos_lens = pos_lens.cuda()
+
+        # Trim
+        pos_samples = helpers.trim_trailing_paddings(pos_samples, pos_lens)
+
+        return pos_samples, pos_lens, cond_ids, end_of_dataset
+
+    def fetch_cond_samples(self, cond_ids):
+        """
+        Return the batch of cond_samples with cond_ids.
+
+        Inputs:
+            - cond_ids: batch_size
+        Outputs: cond_samples, cond_lens
+            - cond_samples: batch_size x seq_len
+            - cond_lens: batch_size
+        """
+        cond_samples, cond_lens = self.cond_samples[cond_ids], self.cond_lens[cond_ids]
+
+        # Wrap in variable
+        cond_samples = autograd.Variable(torch.LongTensor(cond_samples))
+        cond_lens = autograd.Variable(torch.LongTensor(cond_lens))
+        
+        if self.gpu:
+            cond_samples = cond_samples.cuda()
+            cond_lens = cond_lens.cuda()
+        
+        # Trim
+        cond_samples = helpers.trim_trailing_paddings(cond_samples, cond_lens)
+
+        return cond_samples, cond_lens
 
     def sent_to_ints(self, s):
         """
@@ -101,20 +156,51 @@ class DataLoader:
         """
         Samples the dataset, returns num_samples raw samples, starting from the next of last fetched samples.
 
-        Outputs: dataX[i:j], dataY[i:j], end_of_dataset
-            - dataX[i:j]: num_samples_fetched x seq_len (variable size)
-            - dataY[i:j]: num_samples_fetched x seq_len (variable size)
+        Outputs: cond_samples[i:j], pos_samples[i:j], end_of_dataset
+            - cond_samples[i:j]: num_samples_fetched x seq_len
+            - pos_samples[i:j]: num_samples_fetched x seq_len
         """
         i = 0
-        n = len(self.dataX)
+        n = len(self.cond_samples)
         while i < n:
             num_samples = yield
-
             j = min(i + num_samples, n)
-            yield (self.dataX[i:j], self.dataY[i:j], j == n)
+            sample_idx = list(range(i, j))
+            
+            # Check if overlap with frozen batch
+            if self.frozen is not None:
+                start, end = self.frozen
+                if i < start and j > start:
+                    remaining_after_end = num_samples - (start - i)
+                    j = min(end + 1 + remaining_after_end, n)
+                    sample_idx = list(range(i, start)) + list(range(end + 1, j))
+                elif i >= start and i <= end:
+                    j = min(end + 1 + num_samples, n)
+                    sample_idx = list(range(end + 1, j))
 
+            # Fetch
+            yield (self.pos_samples[sample_idx], self.pos_lens[sample_idx], sample_idx, j == n)
+
+            # Move pointer
             if j == n: # reset
                 i = 0
                 # TODO: shuffle here
             else: # continue
                 i = j
+
+    def freeze(self, start, end):
+        """
+        Freeze samples so that they won't be fetched.
+        Only one batch can be frozen at a time.
+        """
+        self.frozen = (start, end) 
+
+    def release(self):
+        """
+        Unfreeze samples.
+        """
+        self.frozen = None
+    
+    def reset(self):
+        self.fetcher = self.fetch()
+        self.frozen = None
--- src/discriminator.py
+++ src/discriminator.py
@@ -1,67 +1,117 @@
+"""
+BiGRU text classifier.
+"""
 import torch
 import torch.autograd as autograd
 import torch.nn as nn
 import torch.nn.functional as F
 import pdb
 
+from . import helpers
+
 class Discriminator(nn.Module):
-    def __init__(self, embedding_dim, hidden_dim, vocab_size, max_seq_len, gpu=False, dropout=0.2):
-        super(Discriminator, self).__init__()
+    def __init__(self, embedding_dim, hidden_dim, vocab_size, max_seq_len=30, end_token=1, pad_token=0, gpu=False, dropout=0.2):
+        super().__init__()
+
         self.hidden_dim = hidden_dim
         self.embedding_dim = embedding_dim
         self.max_seq_len = max_seq_len
+        self.vocab_size = vocab_size
         self.gpu = gpu
+        self.end_token = end_token
+        self.pad_token = pad_token
 
-        self.embeddings = nn.Embedding(vocab_size, embedding_dim)
-        self.gru = nn.GRU(embedding_dim, hidden_dim, num_layers=2, bidirectional=True, dropout=dropout)
-        self.gru2hidden = nn.Linear(2*2*hidden_dim, hidden_dim)
+        # Inp
+        self.gru = nn.GRU(embedding_dim, hidden_dim, num_layers=2, bidirectional=True, dropout=dropout, batch_first=True)
+        self.gru2hidden = nn.Linear(2 * 2 * hidden_dim, hidden_dim)
         self.dropout_linear = nn.Dropout(p=dropout)
-        self.hidden2out = nn.Linear(hidden_dim, 1)
+
+        # Condition
+        self.gru_cond = nn.GRU(embedding_dim, hidden_dim, num_layers=2, bidirectional=True, dropout=dropout, batch_first=True)
+        self.gru2hidden_cond = nn.Linear(2 * 2 * hidden_dim, hidden_dim)
+        self.dropout_linear_cond = nn.Dropout(p=dropout)
+
+        # Shared
+        self.embeddings = nn.Embedding(vocab_size, embedding_dim)
+        self.hidden2out = nn.Linear(hidden_dim * 2, 1)
 
     def init_hidden(self, batch_size):
-        h = autograd.Variable(torch.zeros(2*2*1, batch_size, self.hidden_dim))
+        h = autograd.Variable(torch.zeros(2 * 2 * 1, batch_size, self.hidden_dim))
+        return h.cuda() if self.gpu else h
 
-        if self.gpu:
-            return h.cuda()
-        else:
-            return h
+    def forward(self, inp, inp_lens, cond, cond_lens, hidden, hidden_cond):
+        """
+        Embeds input and applies GRU one batch at a time.
 
-    def forward(self, input, hidden):
-        # input dim                                                # batch_size x seq_len
-        emb = self.embeddings(input)                               # batch_size x seq_len x embedding_dim
-        emb = emb.permute(1, 0, 2)                                 # seq_len x batch_size x embedding_dim
-        _, hidden = self.gru(emb, hidden)                          # 4 x batch_size x hidden_dim
-        hidden = hidden.permute(1, 0, 2).contiguous()              # batch_size x 4 x hidden_dim
-        out = self.gru2hidden(hidden.view(-1, 4*self.hidden_dim))  # batch_size x 4*hidden_dim
+        Inputs:
+            - inp, cond: batch_size x seq_len
+            - inp_lens, cond_lens: batch_size
+            - hidden: 4 x batch_size x hidden_dim
+        """
+        # Inp
+        inp, inp_lens, sort_idx = helpers.sort_sample_by_len(inp, inp_lens)      # sort
+
+        emb = self.embeddings(inp)                                               # batch_size x seq_len x embedding_dim
+
+        inp = nn.utils.rnn.pack_padded_sequence(inp, inp_lens, batch_first=True)
+        _, hidden = self.gru(emb, hidden)                                        # 4 x batch_size x hidden_dim
+        inp, _ = nn.utils.rnn.pad_packed_sequence(inp, batch_first=True)
+
+        out = self.gru2hidden(hidden.view(-1, 4 * self.hidden_dim))              # batch_size x (4 * hidden_dim)
         out = torch.tanh(out)
         out = self.dropout_linear(out)
-        out = self.hidden2out(out)                                 # batch_size x 1
+
+        inp, inp_lens = inp[sort_idx], inp_lens[sort_idx]                        # unsort
+
+        # Cond
+        cond, cond_lens, sort_idx_cond = helpers.sort_sample_by_len(cond, cond_lens)
+
+        emb_cond = self.embeddings(cond)
+
+        cond = nn.utils.rnn.pack_padded_sequence(cond, cond_lens, batch_first=True)
+        _, hidden_cond = self.gru_cond(emb_cond, hidden_cond)
+        cond, _ = nn.utils.rnn.pad_packed_sequence(cond, batch_first=True)
+
+        out_cond = self.gru2hidden_cond(hidden_cond.view(-1, 4 * self.hidden_dim))
+        out_cond = torch.tanh(out_cond)
+        out_cond = self.dropout_linear_cond(out_cond)
+
+        cond, cond_lens = cond[sort_idx_cond], cond_lens[sort_idx_cond]
+
+        # Combine
+        out = torch.cat([out, out_cond], dim=1) # batch_size x (hidden_dim * 2)
+        out = self.hidden2out(out)              # batch_size x 1
         out = torch.sigmoid(out)
+        
         return out
 
-    def batchClassify(self, inp):
+    def batchClassify(self, inp, inp_lens, cond, cond_lens):
         """
         Classifies a batch of sequences.
 
-        Inputs: inp
-            - inp: batch_size x seq_len
+        Inputs:
+            - inp, cond: batch_size x seq_len
+            - inp_lens, cond_lens: batch_size
 
         Returns: out
             - out: batch_size ([0,1] score)
         """
-        h = self.init_hidden(inp.size()[0])
-        out = self.forward(inp, h)
+        batch_size = inp.shape[0]
+        h = self.init_hidden(batch_size)
+        h_cond = self.init_hidden(batch_size)
+        out = self.forward(inp, inp_lens, cond, cond_lens, h, h_cond)
         return out.view(-1)
 
-    def batchBCELoss(self, inp, target):
+    def batchBCELoss(self, inp, inp_lens, cond, cond_lens, target):
         """
         Returns Binary Cross Entropy Loss for discriminator.
 
-         Inputs: inp, target
-            - inp: batch_size x seq_len
+         Inputs:
+            - inp, cond: batch_size x seq_len
+            - inp_lens, cond_lens: batch_size
             - target: batch_size (binary 1/0)
         """
-        loss_fn = nn.BCELoss()
-        h = self.init_hidden(inp.size()[0])
-        out = self.forward(inp, h)
-        return loss_fn(out, target)
+        out = self.batchClassify(inp, inp_lens, cond, cond_lens) 
+        acc = torch.sum((out > 0.5) == (target > 0.5)).data.item()
+        loss = F.binary_cross_entropy(out, target)
+        return loss, acc
