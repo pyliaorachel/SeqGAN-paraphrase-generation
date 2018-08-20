f7047dad0498b94128e6a1f2801565a8bc5ab82a
Rachel P.Y. Liao <pyliao@stanford.edu>
2018-08-20 15:38:02 +0800

    Update training static settings

--- src/utils/hyper_params.py
+++ src/utils/hyper_params.py
@@ -5,13 +5,13 @@ MAX_SEQ_LEN_PADDING = 10
 BATCH_SIZE = 16
 ROLLOUT_NUM = 3
 TEACHER_FORCING_RATIO = 0.6
-G_PRETRAIN_EPOCHS = 1 if DEBUG else 50
+G_PRETRAIN_EPOCHS = 1 if DEBUG else 10
 D_PRETRAIN_STEPS = 1 if DEBUG else 3
-D_PRETRAIN_EPOCHS = 1 if DEBUG else 2
+D_PRETRAIN_EPOCHS = 2 if DEBUG else 2
 G_TRAIN_STEPS = 1 if DEBUG else 1
 D_TRAIN_STEPS = 1 if DEBUG else 3
 D_TRAIN_EPOCHS = 1 if DEBUG else 2
-ADV_TRAIN_ITERS = 3 if DEBUG else 20
+ADV_TRAIN_ITERS = 1 if DEBUG else 20
 
 # Embedding dim must be one of those in pretrained word embeddings: 25, 50, 100, 200
 ED = 50 # embedding dim
